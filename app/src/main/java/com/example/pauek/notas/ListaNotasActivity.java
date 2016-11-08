93cfb240d857b7e13d13f9c5cbaf30ae4d35b25c
Pau Fernández <pau.duran@gmail.com>
2016-11-08 10:24:46 +0100

    Implementamos un Adaptador para mostrar items con layout propio.

--- app/src/main/java/com/example/pauek/notas/ListaNotasActivity.java
+++ app/src/main/java/com/example/pauek/notas/ListaNotasActivity.java
@@ -1,9 +1,14 @@
 package com.example.pauek.notas;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -19,13 +24,33 @@ public class ListaNotasActivity extends AppCompatActivity {
 
         notas = new ArrayList<>();
         notas.add(new Nota("hola", "que tal"));
-        notas.add(new Nota("1234", "como estamos"));
+        notas.add(new Nota("1234", "como estamos asdfasdlfjasldkfj asdlkfjasldkfjasdlfkjasdlfkjasdlfkjas dflkasjdf alskdfjasldkfjasdlfkjasdflkajsdflkasdjfalskdfjasdlfkj "));
 
         lista_notas = (ListView) findViewById(R.id.lista_notas);
-        lista_notas.setAdapter(new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                notas
-        ));
+        lista_notas.setAdapter(new NotasAdapter());
+    }
+
+    class NotasAdapter extends ArrayAdapter<Nota> {
+        public NotasAdapter() {
+            super(ListaNotasActivity.this, R.layout.nota, notas);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                view = inflater.inflate(R.layout.nota, parent, false);
+            }
+
+            Nota nota = getItem(i);
+            TextView titulo = (TextView) view.findViewById(R.id.titulo);
+            titulo.setText(nota.getTitulo());
+            TextView resumen = (TextView) view.findViewById(R.id.resumen);
+            resumen.setText(nota.getTexto());
+
+            return view;
+        }
     }
 }
